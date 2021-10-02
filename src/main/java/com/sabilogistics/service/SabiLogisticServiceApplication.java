7d77d69530fcb4aa538ba7b2b7db5dfc0efdfe97
Davyjones <ekenedavidson15@gmail.com>
2021-10-02 04:53:02 +0100

    new file

---
+++ src/main/java/com/sabilogistics/service/SabiLogisticServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sabilogistics.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabiLogisticServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabiLogisticServiceApplication.class, args);
+	}
+
+}
