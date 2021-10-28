d2b5511db287f823a0af3f9b01043b4cbe89eab1
Davyjones <ekenedavidson15@gmail.com>
2021-10-28 18:29:08 +0100

    signup

---
+++ src/main/java/com/sabi/logistics/service/repositories/PartnerAssetTypeRepository.java
@@ -0,0 +1,8 @@
+package com.sabi.logistics.service.repositories;
+
+import com.sabi.logistics.core.models.PartnerAssetType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartnerAssetTypeRepository extends JpaRepository<PartnerAssetType, Long> {
+}
+
