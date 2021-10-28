a6d2589e7f8fc0e9d719c85bc936a59dfd9b0863
Davyjones <ekenedavidson15@gmail.com>
2021-10-28 19:02:08 +0100

    update merge

---
+++ src/main/java/com/sabi/logistics/service/repositories/BrandRepository.java
@@ -0,0 +1,21 @@
+package com.sabi.logistics.service.repositories;
+
+import com.sabi.logistics.core.models.Brand;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
+
+    Brand findByName(String name);
+
+    @Query("SELECT c FROM Brand c WHERE ((:name IS NULL) OR (:name IS NOT NULL AND c.name = :name))")
+    Page<Brand> findBrand(String name, Pageable pageRequest);
+
+    List<Brand> findByIsActive(Boolean isActive);
+}
--- src/main/java/com/sabi/logistics/service/repositories/PartnerAssetTypeRepository.java
+++ src/main/java/com/sabi/logistics/service/repositories/PartnerAssetTypeRepository.java
@@ -1,8 +1,19 @@
 package com.sabi.logistics.service.repositories;
 
 import com.sabi.logistics.core.models.PartnerAssetType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PartnerAssetTypeRepository extends JpaRepository<PartnerAssetType, Long> {
+
+    List<PartnerAssetType> findByIsActive(Boolean isActive);
+    PartnerAssetType findByAssetTypeId(Long name);
+
+    @Query("SELECT s FROM PartnerAssetType s WHERE ((:partnerId IS NULL) OR (:partnerId IS NOT NULL AND s.partnerId = :partnerId))")
+    Page<PartnerAssetType> findPartnerAssetType(Long partnerId, Pageable pageRequest);
 }
 
