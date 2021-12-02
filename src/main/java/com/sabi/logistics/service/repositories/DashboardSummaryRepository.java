2370bf62e3320e4602d87c1a8e3e8f334fcd8fb0
Gbeminiyi Ariyo-Agbaje <gbeminiyi.ariyo@longbridgetech.com>
2021-12-02 08:38:03 +0100

    +++ Fixes

---
+++ src/main/java/com/sabi/logistics/service/repositories/DashboardSummaryRepository.java
@@ -0,0 +1,18 @@
+package com.sabi.logistics.service.repositories;
+
+
+
+
+import com.sabi.logistics.core.models.DashboardSummary;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface DashboardSummaryRepository extends JpaRepository<DashboardSummary, Long>, JpaSpecificationExecutor<DashboardSummary> {
+
+
+
+
+}
