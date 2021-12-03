aaa20e8356fd05186375493b194fcd556fbd79c6
Davyjones <ekenedavidson15@gmail.com>
2021-12-03 05:49:03 +0100

    new

--- src/main/java/com/sabi/logistics/service/repositories/DashboardSummaryRepository.java
+++ src/main/java/com/sabi/logistics/service/repositories/DashboardSummaryRepository.java
@@ -1,16 +1,13 @@
 package com.sabi.logistics.service.repositories;
 
 
-
-
 import com.sabi.logistics.core.models.DashboardSummary;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface DashboardSummaryRepository extends JpaRepository<DashboardSummary, Long>, JpaSpecificationExecutor<DashboardSummary> {
+public interface DashboardSummaryRepository extends JpaRepository<DashboardSummary, Long> {
 
 
 
